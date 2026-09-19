dfc72fe5fa725d87f2da44aef8dc0d992d148f1b
Willfredy742 <wilfredy742@gmail.com>
2026-09-19 16:07:53 +0200

    feat: add item registration loop

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ print("=" * 60)
 catalog = []
 categories = set()
 allowed_statuses = ["disponible", "reservada", "vendida"]
+
+print("\nPlease enter information for 10 collectible items:")
+
+for i in range(1, 11):
+    print(f"\n--- Registering item {i} of 10 ---")
