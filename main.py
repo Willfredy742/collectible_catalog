fc9317d7d5820099950838a890c1465231494261
Willfredy742 <wilfredy742@gmail.com>
2026-09-19 16:05:45 +0200

    add catalog and status list

--- main.py
+++ main.py
@@ -2,3 +2,7 @@
 print("=" * 60)
 print("      BIENVENIDO AL SISTEMA DE CATÁLOGO DE COLECCIONABLES       ")
 print("=" * 60)
+
+catalog = []
+categories = set()
+allowed_statuses = ["disponible", "reservada", "vendida"]
